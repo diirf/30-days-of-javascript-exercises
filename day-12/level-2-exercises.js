ff9f5e2743ebab16c9404cebe49596360324d6bf
diirf <freinva21@gmail.com>
2024-01-09 17:33:39 +0100

    adding level 2 and 3 exercises of day 12

--- day-12/level-2-exercises.js
+++ day-12/level-2-exercises.js
@@ -1,1 +1,29 @@
 // 1. Escriba una función llamada tenMostFrequentWords que obtenga las diez palabras más frecuentes de una cadena?
+const paragraph = `I love teaching. If you do not love teaching what else can you love. I love Python if you do not love something which can give you all the capabilities to develop an application what else can you love.`;
+
+function tenMostFrequentWords(parag, num){
+    const regex = /\./g;
+    const paragClean = parag.replace(regex, '');
+    const arrayParagClean = paragClean.split(' ');
+
+    const countParag = arrayParagClean.reduce((acc, curr) =>{
+        acc[curr] = (acc[curr] || 0) + 1;
+
+        return acc;
+    }, {});
+
+    const arrayParag = Object.entries(countParag);
+    const orderParag = arrayParag.sort((a, b) => b[1] - a[1]);
+
+    let objectParag = {};
+    let arrayOfObjects = [];
+
+    for (let i = 0; i < num; i++){
+        objectParag = {word: orderParag[i][0], count: orderParag[i][1]};
+        arrayOfObjects.push(objectParag);
+    }
+    
+    return arrayOfObjects;
+}
+
+tenMostFrequentWords(paragraph, 10);
--- day-12/level-3-exercises.js
+++ day-12/level-3-exercises.js
@@ -1,2 +1,29 @@
 // 1. Escribe una función que limpie el texto. Limpia el siguiente texto. Después de la limpieza, cuente tres palabras más frecuentes en la cadena
+const sentence = `%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?`;
 
+function cleanText(senten){
+    const regex = /[%@#&$.,;!?]+/g
+    const cleanSentences = senten.replace(regex,"");
+    const arraySentences = cleanSentences.split(/\s+/);
+
+    const countWords = arraySentences.reduce((acc, curr) => {
+        acc[curr] = (acc[curr] || 0) + 1;
+
+        return acc;
+    },{});
+
+    const arrayCountWords = Object.entries(countWords);
+    const orderWord = arrayCountWords.sort((a,b) => b[1] - a[1]);
+    
+    let objectWord = {};
+    let arrayWord = [];
+
+    for (let i = 0; i < 3; i++){
+        objectWord = {word: orderWord[i][0], count: orderWord[i][1]};
+        arrayWord.push(objectWord);
+    }
+
+    return arrayWord;
+}
+
+cleanText(sentence);
