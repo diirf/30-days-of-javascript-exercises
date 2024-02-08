eeec8aaceeef3e644f62262e84dbe5294072a249
diirf <freinva21@gmail.com>
2024-02-08 12:16:07 +0100

    adding exercises level 1 and 2

--- day-19/level-1-exercises.js
+++ day-19/level-1-exercises.js
@@ -1,1 +1,17 @@
 // 1. Crear una closure que tenga una función interna
+function sum(num1, num2){
+    const total = num1 + num2;
+  
+    function multiply(quantity){
+      if (quantity > 0){
+        return total * quantity;
+      }
+    }
+  
+    return multiply;
+  }
+  
+  const closure = sum(1,1);
+
+  closure(1);
+  closure(3);
--- day-19/level-2-exercises.js
+++ day-19/level-2-exercises.js
@@ -1,1 +1,37 @@
 // 1. Crear una closure que tenga tres funciones internas
+function calc(init){
+  let num = init || 0;
+
+  return{
+    add: function add(num1, num2){
+      if (num2){
+        return (num += num1 + num2);
+      }
+      else{
+        return (num += num1);
+      }
+    },
+    minus: function minus(num1, num2){
+      if (num2){
+        return (num = num - num1 - num2);
+      }
+      else{
+        return (num -= num1);
+      }
+    },
+    multiply: function multiply(num1, num2){
+      if (num2){
+        return (num = num * num1 * num2);
+      }
+      else{
+        return (num *= num1);
+      }
+    },
+  };
+}
+
+const calculator = calc(5);
+
+calculator.add(1,1);
+calculator.minus(25);
+calculator.multiply(10,3);
